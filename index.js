292f9e370e99fdff7f6f571c14156bff2c2f974d
sluongo718 <sluongo718@yahoo.com>
2021-06-21 17:00:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
  function wakeDog(a, b) {
-   return ('Wake ${a} the ${b}');
+   return `Wake ${a} the ${b}`;
+}
+
+function leashDog(a, b) {
+    return `Leash ${a} the ${b}`;
+}
+
+function walkToPark(a, b) {
+    return `Walk to the park with ${a} the ${b}`;
+}
+
+function throwFrisbee(a, b) {
+    return `Throw the frisbee for ${a} the ${b}`
+}
+
+function walkHome(a, b) {
+    return `Walk home with ${a} the ${b}`
+}
+
+function unleashDog(a, b) {
+    return `Unleash ${a} the ${b}`
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    let routineEvent = []
+    routine.forEach(event => {
+        routineEvent.push(event(dogName, dogBreed))
+    })
+    return routineEvent
 }
